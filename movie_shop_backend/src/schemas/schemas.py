9b9d95b7c0fbd655143c3f0ab3d4bd7f57bd3c06
Federico <fglmr95@gmail.com>
2025-08-13 15:09:59 -0300

    Creación de esquemas

--- movie_shop_backend/src/schemas/schemas.py
+++ movie_shop_backend/src/schemas/schemas.py
@@ -1,14 +1,39 @@
 from typing import List, Optional
 from pydantic import BaseModel
 
+# Modelo 
 class Movie(BaseModel):
+    id: int
+    name: str
+    director: str
+    gender: List[str] #pasamos todos los parametros x upper desp 
+    shop : int
+    rent: False #booleano predeterminado false.
 
+# DTO(s)
 class MovieRequestCreate(BaseModel):
+    name: str
+    director: str
+    gender: List[str]
 
 class MovieRequestUpdate(BaseModel):
+    name: str
+    director: str
+    gender: List[str]
 
+# Modelo
 class Shop(BaseModel):
+    id: int
+    address: str
+    manager: str
+    movies: List[Movie]
 
+# DTO(s)
 class ShopRequestCreate(BaseModel):
+    address: str
+    manager: str
+
 
 class ShopRequestUpdate(BaseModel):
+    address: str
+    manager: str
